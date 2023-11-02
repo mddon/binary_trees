60688e3b29ad31e5867cf98c1f652eafae83c45a
Miracle Obinna <mmrealworld@yahoo.com>
2023-11-02 19:16:48 +0000

    binary_tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,10 +7,13 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
 
 	size_t size = 1;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
 
 	size += binary_tree_size(tree->right);
 	size += binary_tree_size(tree->left);
