6499fab841a76549747fe19acf30f6ddaf6b5ae9
Miracle Obinna <mmrealworld@yahoo.com>
2023-11-02 19:32:22 +0000

    binary_tree

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - Counts the nodes in a binary tree
+ * @tree: A pointer to the root node of the binary tree
+ * Return: Number of nodes in a binary tree
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	size_t nodes = 0;
+
+	if (tree->left || tree->right)
+		nodes += 1;
+
+	nodes += binary_tree_nodes(tree->left);
+	nodes += binary_tree_nodes(tree->right);
+
+	return (nodes);
+}
