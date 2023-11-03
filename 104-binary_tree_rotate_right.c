7f6e6e9b7817ea25e13b72311d697f9d191b0459
Miracle Obinna <mmrealworld@yahoo.com>
2023-11-03 20:54:30 +0000

    binary_tree

---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+	binary_tree_t *new_root;
+
+	if (tree == NULL || tree->left == NULL)
+	{
+		return (NULL);
+	}
+	new_root = tree->left;
+	tree->left = new_root->right;
+	if (new_root->right != NULL)
+	{
+		new_root->right->parent = tree;
+	}
+	new_root->right = tree;
+	new_root->parent = tree->parent;
+	tree->parent = new_root;
+	return (new_root);
+}
